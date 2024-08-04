887cf2315a11191b4bd79033130ce845a8ce2170
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-08-04 15:03:51 -0300

    refactor: move label inside input fields

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
-import { Eye, EyeOff } from "lucide-react"
+import { Eye, EyeOff, Lock, Mail } from "lucide-react"
 
 const schema = z.object({
   email: z.string().email(),
@@ -44,13 +44,13 @@ export const Login = () => {
     <div className="h-screen bg-slate-100 text-black">
       <div className="flex h-full flex-col items-center justify-center gap-8">
         <div className="space-y-2">
-          <h1 className="text-center text-3xl font-medium">Welcome back</h1>
+          <h1 className="text-center text-3xl font-semibold">Welcome back</h1>
           <span className="block text-gray-500">
             Welcome back! Please enter your details
           </span>
         </div>
 
-        <div className="w-full space-y-4 px-20">
+        <div className="w-full space-y-4 px-20 sm:max-w-[500px]">
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(onSubmit)}
@@ -60,16 +60,21 @@ export const Login = () => {
                 control={form.control}
                 name="email"
                 render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        {...form.register("email")}
-                        placeholder="example@example.com"
-                        className="h-10 ring-1 ring-gray-300 placeholder:text-sm focus-visible:ring-1 focus-visible:ring-gray-300"
-                      />
-                    </FormControl>
+                  <FormItem className="space-y-1">
+                    <div className="flex h-10 items-center rounded-md px-2 ring-1 ring-gray-300 placeholder:text-sm focus-visible:ring-1 focus-visible:ring-gray-300">
+                      <FormLabel>
+                        <Mail className="size-5 text-gray-600" />
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          {...form.register("email")}
+                          id="email"
+                          placeholder="example@example.com"
+                          className="border-none shadow-none focus-visible:ring-0"
+                        />
+                      </FormControl>
+                    </div>
                   </FormItem>
                 )}
               />
@@ -79,29 +84,31 @@ export const Login = () => {
                 name="password"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <div className="flex h-10 items-center rounded-md border-2 border-gray-300 pr-4">
+                    <div className="flex h-10 items-center rounded-md px-2 ring-1 ring-gray-300 placeholder:text-sm focus-visible:ring-1 focus-visible:ring-gray-300">
+                      <FormLabel>
+                        <Lock className="size-5 text-gray-600" />
+                      </FormLabel>
+                      <FormControl>
                         <Input
-                          id="password"
                           {...field}
                           {...form.register("password")}
                           type={isEyeOpen}
-                          className="border-none shadow-none placeholder:text-sm focus-visible:ring-0"
+                          placeholder="password"
+                          className="border-none shadow-none focus-visible:ring-0"
                         />
-                        {isEyeOpen === "password" ? (
-                          <Eye
-                            className="cursor-pointer"
-                            onClick={changeTypeInputPassword}
-                          />
-                        ) : (
-                          <EyeOff
-                            className="cursor-pointer"
-                            onClick={changeTypeInputPassword}
-                          />
-                        )}
-                      </div>
-                    </FormControl>
+                      </FormControl>
+                      {isEyeOpen === "password" ? (
+                        <Eye
+                          className="size-5 cursor-pointer text-gray-600"
+                          onClick={changeTypeInputPassword}
+                        />
+                      ) : (
+                        <EyeOff
+                          className="size-5 cursor-pointer text-gray-600"
+                          onClick={changeTypeInputPassword}
+                        />
+                      )}
+                    </div>
                   </FormItem>
                 )}
               />
@@ -110,16 +117,19 @@ export const Login = () => {
                 <a href="#">Forgot password</a>
               </div>
 
-              <Button className="h-12 bg-green-600 text-base hover:bg-green-500">
+              <Button
+                type="submit"
+                className="h-12 bg-green-600 text-base hover:bg-green-500"
+              >
                 Sign in
               </Button>
             </form>
           </Form>
 
           <div className="flex items-center gap-2">
-            <div className="h-px w-full bg-gray-500"></div>
-            <div className="whitespace-nowrap">or also</div>
-            <div className="h-px w-full bg-gray-500"></div>
+            <div className="h-px w-full bg-gray-300"></div>
+            <div className="whitespace-nowrap text-sm text-gray-500">OR</div>
+            <div className="h-px w-full bg-gray-300"></div>
           </div>
 
           <Button className="flex h-12 w-full gap-4 bg-slate-100 text-base text-black ring-1 ring-gray-300 hover:bg-slate-200">
@@ -130,7 +140,7 @@ export const Login = () => {
 
         <div className="space-x-2 text-sm">
           <span>Don't have an account?</span>
-          <a href="#">
+          <a href="/register">
             <span className="cursor-pointer font-semibold text-green-600 hover:underline">
               Sign Up
             </span>

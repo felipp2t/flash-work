19de98933d298d0be7c2f6c433a38ca86501fde0
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-11-09 14:34:16 -0300

    feat: add dark mode

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,13 @@
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./_components/theme-provider";
 import { router } from "./router";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Toaster richColors closeButton />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }

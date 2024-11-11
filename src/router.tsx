a370459531ddbbfd23a025100f824fc6e7e7f3bc
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-11-10 21:13:56 -0300

    feat: add routing for Regions and Remotes service pages with RootLayout

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import { SignInPage } from "./pages/(auth)/sign-in/page";
 import { SignUpPage } from "./pages/(auth)/sign-up/page";
+import { RegionsServicePage } from "./pages/(home)/services/regions/page";
+import { RemotesServicePage } from "./pages/(home)/services/remotes/page";
+import { RootLayout } from "./pages/layout";
 
 export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/services/region", element: <RegionsServicePage /> },
+      { path: "/services/remotes", element: <RemotesServicePage /> },
+    ],
+  },
   {
     path: "/sign-in",
     element: <SignInPage />,

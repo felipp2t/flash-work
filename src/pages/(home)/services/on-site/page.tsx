662ee4c430e420faa0975b82ad60f3a0e2c14e5e
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-11-10 21:26:34 -0300

    refactor: update page title for OnSiteServicesPage to enhance clarity

--- src/pages/(home)/services/on-site/page.tsx
+++ src/pages/(home)/services/on-site/page.tsx
@@ -5,7 +5,7 @@ import { OnSite } from "../../_components/services/onsite";
 export const OnSiteServicesPage = () => {
   return (
     <div className="flex flex-1 flex-col gap-16 p-4">
-      <PageTitle title="Serviços - Região" />
+      <PageTitle title="Serviços - Presenciais" />
 
       <div className="flex flex-col gap-16">
         <ServiceList>

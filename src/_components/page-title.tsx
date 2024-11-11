ca6de76d9db3862176894244828f1c5dbc3400b7
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-11-10 21:14:05 -0300

    feat: add PageTitle component for displaying section titles

---
+++ src/_components/page-title.tsx
@@ -0,0 +1,11 @@
+interface SectionTitleProps {
+  title: string;
+}
+
+export const PageTitle = ({ title }: SectionTitleProps) => {
+  return (
+    <h1 className="relative text-4xl font-bold tracking-normal before:absolute before:left-0 before:-bottom-3 before:h-1 before:rounded-full before:w-28 before:bg-primary before:content-['']">
+      {title}
+    </h1>
+  );
+};

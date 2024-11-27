83c8d0e7fb99aec9acc355fa23420a3d1f39f8cd
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2024-11-26 23:24:07 -0300

    feat: implement pagination for categories and update API request

--- src/http/categories/get-categories.ts
+++ src/http/categories/get-categories.ts
@@ -2,13 +2,27 @@ import { CategoryResponse } from "@/@types/categories/category-response";
 import { env } from "@/env";
 import axios from "axios";
 
+interface GetCategoriesRequets {
+  page: number;
+  size: number;
+}
+
 interface GetCategoriesResponse {
   categories: CategoryResponse;
 }
 
-export const getCategories = async (): Promise<GetCategoriesResponse> => {
+export const getCategories = async ({
+  page,
+  size,
+}: GetCategoriesRequets): Promise<GetCategoriesResponse> => {
+  const params = {
+    page: page - 1,
+    size,
+  };
+
   const { data: categories }: { data: CategoryResponse } = await axios.get(
     `${env.BACKEND_ENDPOINT}/categories`,
+    { params },
   );
 
   return { categories };
--- src/pages/admin/categories/components/categories-table.tsx
+++ src/pages/admin/categories/components/categories-table.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "@/components/pagination";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -9,52 +10,74 @@ import {
 } from "@/components/ui/table";
 import { getCategories } from "@/http/categories/get-categories";
 import { useQuery } from "@tanstack/react-query";
-import { Package, Trash2 } from "lucide-react";
+import * as Icons from "lucide-react";
+import { Trash2 } from "lucide-react";
+import { useSearchParams } from "react-router-dom";
 import { EditCategoryButton } from "./edit-transaction-button";
 
 export const AdminCategoriesTable = () => {
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+
+  const perPage = searchParams.get("per_page")
+    ? Number(searchParams.get("per_page"))
+    : 10;
+
   const { data } = useQuery({
-    queryKey: ["get-categories"],
-    queryFn: async () => await getCategories(),
+    queryKey: ["get-categories", page, perPage],
+    queryFn: async () => await getCategories({ page, size: perPage }),
     staleTime: 1000 * 60 * 15,
   });
 
   if (!data) return;
 
+  const getIconComponent = (iconName: string) => {
+    const icons = Icons as unknown as Record<string, React.ElementType>;
+    return icons[iconName] || Icons.Package;
+  };
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Icon</TableHead>
-          <TableHead>Nome</TableHead>
-          <TableHead>Descrição</TableHead>
-          <TableHead>Ações</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {data.categories.content.map((category) => {
-          const Icon = category.iconName || Package;
-          return (
-            <TableRow key={category.id}>
-              <TableCell>
-                <Icon className="size-6" />
-              </TableCell>
-              <TableCell className="font-medium capitalize">
-                {category.name}
-              </TableCell>
-              <TableCell>{category.description}</TableCell>
-              <TableCell>
-                <div className="flex space-x-2">
-                  <EditCategoryButton category={category} />
-                  <Button variant="outline" size="icon">
-                    <Trash2 className="size-4" />
-                  </Button>
-                </div>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Icon</TableHead>
+            <TableHead>Nome</TableHead>
+            <TableHead>Descrição</TableHead>
+            <TableHead>Ações</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.categories.content.map((category) => {
+            const Icon = getIconComponent(category.iconName);
+            return (
+              <TableRow key={category.id}>
+                <TableCell>
+                  <Icon className="size-6" />
+                </TableCell>
+                <TableCell className="font-medium capitalize">
+                  {category.name}
+                </TableCell>
+                <TableCell>{category.description}</TableCell>
+                <TableCell>
+                  <div className="flex space-x-2">
+                    <EditCategoryButton category={category} />
+                    <Button variant="outline" size="icon">
+                      <Trash2 className="size-4" />
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+
+      <Pagination
+        items={data.categories.totalElements}
+        page={page}
+        pages={data.categories.totalPages}
+      />
+    </>
   );
 };
